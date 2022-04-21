7169c2d435b738e90c356e686da720e3cf9ae2ad
Alexander Au <57201876+mechdragon23@users.noreply.github.com>
2022-04-21 15:33:10 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <myHeader.h>
+
+int main () 
+{
+    int menuSelection;
+
+    cout << "Welcome to Gamba\n";
+    cout << "Select a game:\n\n";
+
+    cout << "Heads or Tails - 1\n";
+    cout << "Horse racing --- 2\n";
+    cout << "Dice ----------- 3\n";
+    cout << "Scratchers ----- 4\n";
+
+    cout << "\nquit ----------- 0\n";
+
+    cout << "\nEnter the number of the game you would like to play: ";
+    cin  >> menuSelection;
+
+    while(menuSelection != 0)
+    {
+        switch(menuSelection)
+        {
+            case 1:
+                //heads or tails function here
+                break;
+            case 2:
+                //horse racing function here
+                break;
+            case 3:
+                //dice function here
+                break;
+            case 4:
+                //scratchers function here
+                break;
+        }
+
+
+        system ("CLS"); //clear screen
+        cout << "Select a game:\n\n";
+
+        cout << "Heads or Tails - 1\n";
+        cout << "Horse racing --- 2\n";
+        cout << "Dice ----------- 3\n";
+        cout << "Scratchers ----- 4\n";
+
+        cout << "\nquit ----------- 0\n";
+
+        cout << "\nEnter the number of the game you would like to play: ";
+        cin  >> menuSelection;
+    }
+
+
+	return 0;
+}
---
+++ myHeader.h
@@ -0,0 +1,14 @@
+#ifndef MY_HEADER
+#define MY_HEADER
+
+#include <cstdlib>
+#include <stdlib.h>
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+
+#endif
